014d5f7ed4da0e1a551e6600f19ea844bc638b02
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2025-12-14 10:59:38 +0100

    Split taskfile generation

--- flake.nix
+++ flake.nix
@@ -84,6 +84,8 @@
           };
           modules = hostModules name cfg;
         };
+
+      tasks = import ./nix/lib/tasks.nix;
     in
     {
       # Generate NixOS configurations for all hosts
@@ -92,105 +94,14 @@
     // (flake-utils.lib.eachDefaultSystem (
       system:
       let
-        # Creave an overlay to use Determinate's nix in nixos-rebuild-ng
-        nixos-rebuild-ng-overlay = final: prev: {
-          nixos-rebuild-ng = prev.nixos-rebuild-ng.override {
-            # Swap the upstream 'nix' with the Determinate one
-            nix = determinate.packages.${system}.default // {
-              # Manually satisfy the version check
-              version = "2.18";
-            };
-          };
-        };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ nixos-rebuild-ng-overlay ];
-        };
-        mkCmd = name: ''
-          ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild-ng \
-            --flake .#${name} \
-            --target-host root@${name} \
-            --build-host root@${name} \
-            {{.CLI_ARGS}}
-        '';
-        taggedHosts = lib.foldl' (
-          acc: hostName:
-          let
-            hostTags = hosts.${hostName}.tags or [ ];
-          in
-          lib.foldl' (
-            innerAcc: tag:
-            innerAcc
-            // {
-              ${tag} = (innerAcc.${tag} or [ ]) ++ [ hostName ];
-            }
-          ) acc hostTags
-        ) { } (lib.attrNames hosts);
-        taskfile = {
-          version = "3";
-          output = "prefixed";
-          tasks = {
-            default = {
-              desc = "Check flake";
-              cmd = "nix flake check --no-build";
-            };
-            synth = {
-              desc = "Synthesize K8s manifests with cdk8s";
-              cmd = "cdk8s synth";
-              sources = [ "k8s/**/*" ];
-              generates = [ "dist/**/*.yaml" ];
-            };
-            deploy = {
-              desc = "Deploy reef k8s cluster with kapp";
-              sources = [ "dist/**/*" ];
-              deps = [ "synth" ];
-              cmd = "kapp app-group deploy -g cdk8s -d ./dist --yes";
-            };
-          }
-          # Per tag group tasks
-          // lib.mapAttrs' (tag: hostNames: {
-            name = "@${tag}";
-            value = {
-              desc = "Deploy configuration to all ${tag} hosts";
-              deps = hostNames;
-            };
-          }) taggedHosts
-          # Per host tasks
-          // lib.mapAttrs (name: cfg: {
-            desc = "Deploy configuration to ${name}";
-            cmds = [
-              (mkCmd name)
-            ];
-          }) hosts;
-        };
-        yamlFormat = pkgs.formats.yaml { };
-        taskfilePackage = pkgs.stdenv.mkDerivation {
-          name = "task";
-          taskfileYaml = yamlFormat.generate "Taskfile.yml" taskfile;
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out/share/taskfiles $out/bin
-            cp "$taskfileYaml" $out/share/taskfiles/Taskfile.yml
-            cat > $out/bin/task <<EOF
-              #!${pkgs.runtimeShell}
-              set -euo pipefail
-              exec ${pkgs.go-task}/bin/task -d ./ --taskfile "$out/share/taskfiles/Taskfile.yml" "\$@"
-            EOF
-            chmod +x $out/bin/task
-          '';
-        };
-        taskfileApp = {
-          type = "app";
-          program = "${taskfilePackage}/bin/task";
+        pkgs = import nixpkgs { inherit system; };
+        tasksForSystem = tasks {
+          inherit nixpkgs lib hosts determinate system;
         };
       in
       {
-        packages = {
-          default = taskfilePackage;
-        };
-        apps = {
-          taskfile = taskfileApp;
-        };
+        packages = tasksForSystem.packages;
+        apps = tasksForSystem.apps;
         devShells = {
           default = pkgs.mkShellNoCC {
             packages =
@@ -200,7 +111,7 @@
                 kapp
                 vtsls
               ]
-              ++ [ taskfilePackage ];
+              ++ [ tasksForSystem.packages.default ];
           };
         };
       }
