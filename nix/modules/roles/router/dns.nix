5dcb10e9d814e06c7367878e8caaa18627cd5fcb
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2025-12-24 16:44:09 +0100

    --wip-- [skip ci]

--- nix/modules/roles/router/dns.nix
+++ nix/modules/roles/router/dns.nix
@@ -60,6 +60,13 @@
               private_networks = [ "192.168.127.0/24" ]; # TODO: Move to var
               local_ptr_upstreams = [ config.router.services.dhcp.address ];
               use_private_ptr_resolvers = true;
+              rewrites = map
+                (host: {
+                  answer = host.ip;
+                  domain = host.name;
+                  type = "A";
+                })
+                config.router.services.dns.dnsHosts;
             };
             dhcp = {
               enabled = false;
