7e421651656e5a33c9d0f1b99a2e7a5d1bfcc255
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2026-01-19 23:05:03 +0100

    Add ping metrics

--- nix/modules/roles/router/observability.nix
+++ nix/modules/roles/router/observability.nix
@@ -3,12 +3,27 @@
   ...
 }:
 let
-  blackBoxConfigFormat = pkgs.formats.yaml { };
+  blackboxConfigFormat = pkgs.formats.yaml { };
+  blackboxRelabelConfigs = [
+    {
+      source_labels = [ "__address__" ];
+      target_label = "__param_target";
+    }
+    {
+      source_labels = [ "__param_target" ];
+      target_label = "instance";
+    }
+    {
+      target_label = "__address__";
+      # Blackbox exporter real hostname:port.
+      replacement = "127.0.0.1:9115";
+    }
+  ];
 in
 {
   services.prometheus.exporters.blackbox = {
     enable = true;
-    configFile = blackBoxConfigFormat.generate "config.yaml" {
+    configFile = blackboxConfigFormat.generate "config.yaml" {
       modules = {
         http_2xx = {
           prober = "http";
@@ -23,5 +38,51 @@ in
         };
       };
     };
+  };
+  services.prometheus = {
+    enable = true;
+    scrapeConfigs = [
+      {
+        job_name = "blackbox_icmp";
+        metrics_path = "/probe";
+        scrape_interval = "1s";
+        params = {
+          module = [ "icmp" ];
+        };
+        static_configs = [
+          {
+            targets = [
+              "ssh.munin.xyz"
+              "192.168.1.1"
+              "1.1.1.1"
+              "8.8.8.8"
+              "192.168.127.20"
+            ];
+          }
+        ];
+        relabel_configs = blackboxRelabelConfigs;
+      }
+      {
+        job_name = "blackbox_http";
+        metrics_path = "/probe";
+        scrape_interval = "1s";
+        params = {
+          module = [ "http_2xx" ];
+        };
+        static_configs = [
+          {
+            targets = [
+              "ssh.munin.xyz"
+            ];
+          }
+        ];
+        relabel_configs = blackboxRelabelConfigs;
+
+      }
+      {
+        job_name = "blackbox_exporter";
+        static_configs = [ { targets = [ "127.0.0.1:9115" ]; } ];
+      }
+    ];
   };
 }
