1c9b7cc9d40523e28e8f669d7d000b741d248272
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2026-01-20 10:06:05 +0100

    Enable basic grafana

--- nix/modules/roles/router/observability.nix
+++ nix/modules/roles/router/observability.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   ...
 }:
@@ -16,11 +17,50 @@ let
     {
       target_label = "__address__";
       # Blackbox exporter real hostname:port.
-      replacement = "127.0.0.1:9115";
+      replacement = "127.0.0.1:${toString config.services.prometheus.exporters.blackbox.port}";
     }
   ];
 in
 {
+  services.grafana = {
+    enable = true;
+
+    settings = {
+      server.http_addr = config.router.lan.address;
+
+      "auth.anonymous" = {
+        enabled = true;
+        org_role = "Viewer";
+      };
+
+      analytics.reporting_enabled = false;
+    };
+
+    provision = {
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          url = "http://localhost:${toString config.services.prometheus.port}";
+          isDefault = true;
+        }
+      ];
+    };
+  };
+  services.prometheus.exporters.node = {
+    enable = true;
+    enabledCollectors = [
+      "systemd"
+      "diskstats"
+      "filesystem"
+      "loadavg"
+      "meminfo"
+      "netdev"
+      "stat"
+      "time"
+      "uname"
+    ];
+  };
   services.prometheus.exporters.blackbox = {
     enable = true;
     configFile = blackboxConfigFormat.generate "config.yaml" {
@@ -42,7 +82,19 @@ in
   services.prometheus = {
     enable = true;
     retentionTime = "30d";
+
+    globalConfig = {
+      scrape_interval = "15s";
+      evaluation_interval = "15s";
+    };
+
     scrapeConfigs = [
+      {
+        job_name = "node_exporter";
+        static_configs = [
+          { targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ]; }
+        ];
+      }
       {
         job_name = "blackbox_icmp";
         metrics_path = "/probe";
@@ -82,7 +134,9 @@ in
       }
       {
         job_name = "blackbox_exporter";
-        static_configs = [ { targets = [ "127.0.0.1:9115" ]; } ];
+        static_configs = [
+          { targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.blackbox.port}" ]; }
+        ];
       }
     ];
   };
